a9ebe06eee8543977be5cc95d79e75372ec0db4f
Ngawang P Sherpa <ngawangpsherpa@gmail.com>
2023-07-12 01:30:53 -0400

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,31 @@
 import React, { useEffect, useState } from "react";
 
 function App() {
-  const [data, setData] = useState(null);
-
-  const fetchData = async () => {
-    try {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/todos/1"
-      );
-      const responseData = await response.json();
-      setData(responseData);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const [data, setData] = useState();
 
   useEffect(() => {
-    fetchData();
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/todos/1"
+        )
+          .then((res) => res.json())
+          .then((json) => {
+            console.log(json);
+            setUserData(json);
+          });
+      } catch {
+        console.log("Error fetching userData");
+      }
+    };
+
+    fetchId();
   }, []);
 
   return (
     <div>
-      <h1>{data ? data.title : "Loading.."}</h1>
+      <h1>User Data</h1>
+      {userData && <pre>{JSON.stringify(userData, null, 2)}</pre>}
     </div>
   );
 }
