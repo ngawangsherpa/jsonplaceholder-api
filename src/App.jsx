e2a6ced3d8956c57092b5c5f03526e11f4832414
Ngawang P Sherpa <ngawangpsherpa@gmail.com>
2023-07-12 01:55:38 -0400

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import React, { useEffect, useState } from "react";
 
 function App() {
-  const [data, setData] = useState();
+  const [userData, setUserData] = useState();
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchUserData = async () => {
       try {
         const response = await fetch(
           "https://jsonplaceholder.typicode.com/todos/1"
         )
-          .then((res) => res.json())
+          .then((response) => response.json())
           .then((json) => {
             console.log(json);
             setUserData(json);
           });
       } catch {
-        console.log("Error fetching userData");
+        console.log("Error");
       }
     };
 
-    fetchId();
+    fetchUserData();
   }, []);
 
   return (
@@ -29,5 +29,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
